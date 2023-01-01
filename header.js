9c3adffa62ff7641d22613550f60e9b6bf4ee5bc
koanyan5028 <hikatti.113@gmail.com>
2023-01-02 04:28:20 +0900

    commit

--- header.js
+++ header.js
@@ -2,9 +2,9 @@ function header() {
 	$.ajax({
 		url: "header.html",
 		cache: false,
-		success: function(html) {
-			let element=document.getElementById("header");
+		success: function(html){
 			console.log(html)
+			let element=document.getElementById("header");
 			element.innerHTML=html;
 		}
 	});
