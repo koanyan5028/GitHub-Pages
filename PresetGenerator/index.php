a28fc93fa006434b38b1def3c514956ef750609f
koanyan5028 <hikatti.113@gmail.com>
2023-01-02 03:54:50 +0900

    プロジェクト ファイルを追加します。

---
+++ PresetGenerator/index.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html>
+<!--PresetGenerator-->
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="../style.css"/>
+	</head>
+	<body>
+		<?php include("header.html");?>
+		<div class="body">
+			<h1>プリセット生成機</h1>
+			<p class="bold">
+				ワールドタイプ: カスタマイズのプリセット生成機です
+			</p>
+		</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+<!--Index-->
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+	</head>
+	<body>
+		<?php include("header.html");?>
+		<div class="body">
+			<p class="bold">
+				ワールドタイプ: カスタマイズの<a href="PresetGenerator/index.php">プリセット生成機</a>とかをまとめたサイトです<br />
+				チャンネル登録、ツイッターのフォローなどしてくれると嬉しいです
+			</p>
+			<p class="no_margin_bold">
+				リンク:
+			</p>
+			<div class="links">
+				<p class="no_margin">
+					<a href="https://www.youtube.com/@koanya_game">YouTube</a>
+					<br />
+					<a href="https://twitter.com/koanyan">Twitter</a>
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
